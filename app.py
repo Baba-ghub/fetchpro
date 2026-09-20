2c58b4c1d666209fa294edac7f8efb9db136c366
arhaan <arhaa1@gmail.com>
2026-09-20 07:58:17 +0000

    arhaan-1

--- app.py
+++ app.py
@@ -3,4 +3,6 @@ num2=int(input("Enter a number"))
 print("addition is :", num1+num2)
 print("sub is :", num1-num2)
 print("multiply is :", num1*num2)
+print("division is :", num1/num2)
+print("module is :", num1%num2)
 
